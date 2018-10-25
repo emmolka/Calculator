7dba14edda1821ea38981f6d6ee77fa4071d7f7e
Master <git@github.com:emmolka/NavBar.git>
2018-10-25 21:19:48 +0200

    Calculator

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+let total='';
+$(".button-number").click(function(event){
+    
+    let buttonClicked = this.innerHTML;
+    total=total+buttonClicked;
+    console.log(total)
+    document.querySelector('#score1').textContent = total;
+});
+$(".button").click(function(event){
+    
+    let actionClicked = this.innerHTML;
+    total=total+actionClicked;
+    console.log(total)
+    document.querySelector('#score1').textContent = total;
+});
+$(".button-equal").click(function(event){
+    console.log(eval(total))
+   document.querySelector('#score').textContent = eval(total);
+});
+$('.button-AC').click(function(event){
+   total=0; 
+    document.querySelector('#score1').textContent = total;
+});
+
+});
